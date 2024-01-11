59f52553c43806496d8056384ebe13cff9dda755
HarrySparks <116262821+HarrySparks@users.noreply.github.com>
2024-01-11 10:29:48 +0000

    change graph range

--- main.py
+++ main.py
@@ -20,54 +20,54 @@ window.config(padx=50, pady=10, bg=colour3)
 window.resizable(True, True)
 
 def ShowStockPrice():
-    dateentry = inputdate.get()
-    year, month, day = map(str, dateentry.split('-'))
-    collectivedate = str(year)+"/"+str(month)+"/"+str(day)
+    text = "current price : " + str(math.trunc(stock.price(inputstock.get())*100)/100)
 
-    text = "current price : " + str(math.trunc(stock.getstockprice(inputstock.get())*100)/100) + "\n" + collectivedate + " price : "+ str(math.trunc(stock.getstockpricedate(year,month,day,inputstock.get())*100)/100)
+    if len(inputdate.get()) != 0:
+        dateentry = inputdate.get()
+        year, month, day = map(str, dateentry.split('-'))
+        collectivedate = str(year)+"/"+str(month)+"/"+str(day)
+        text += "\n" + collectivedate + " price : "+ str(math.trunc(stock.pricedate(year,month,day,inputstock.get())*100)/100)
 
     displaylabel.config(text=text)
 
-def ShowStockPriceDate():
-    dateentry = inputdate.get()
-    year, month, day = map(str, dateentry.split('-'))
-    
-    collectivedate=str(year)+" : "+str(month)+" : "+str(day)+" | "
-    displaylabel.config(text=collectivedate+str(math.trunc(stock.getstockpricedate(year,month,day,inputstock.get())*100)/100))
-
-def GetStockDataForSymbol():
+def GetStockGraph():
     for widget in graphframe.winfo_children():
         widget.destroy()
-    stockData = stock.getstockpricedatayear(inputstock.get())
+    stockData = stock.range(inputrange0.get(),inputrange1.get(),inputstock.get())
     graphDrawer.showGraphForDataFrame(inputstock.get(),stockData,graphframe)
 
 mainframe = Frame(window)
 graphframe = Frame(window, height=600) 
 #need to make graph larger
 
-label = Label(mainframe, text="Enter Stock Ticker:")
-label.pack(side=TOP)
-
+stocklabel = Label(mainframe, text="Enter Stock Ticker:")
+stocklabel.pack(side=TOP)
 inputstock = Entry(mainframe, width=35, bg=colour6)
 inputstock.pack(pady = 5, side=TOP)
 
 datelabel = Label(mainframe, text="Enter Stock Date:")
 datelabel.pack(side=TOP)
-
 inputdate = Entry(mainframe, width=35, bg=colour6)
 inputdate.pack(pady = 5, side=TOP)
 
 displaylabel = Label(mainframe, text="", bg=colour6) 
 displaylabel.pack()
 
-getDataButton = Button(mainframe, text="Get Data", command=GetStockDataForSymbol, width=40, height=5, bg=colour5)
+getDataButton = Button(mainframe, text="Get Data", command=GetStockGraph, width=40, height=5, bg=colour5)
 getDataButton.pack(pady=10, padx=10, side=LEFT)
-
-getStockPriceDate = Button(mainframe, text="Get Stock Price At Date",command=ShowStockPriceDate, width=40, height=5, bg=colour5)
-getStockPriceDate.pack(pady=10, padx=10, side=RIGHT)
 
 getStockPrice = Button(mainframe, text="Get Stock Price",command=ShowStockPrice, width=40, height=5, bg=colour5)
 getStockPrice.pack(pady=10, padx=10, side=RIGHT)
+
+labelrange0 = Label(mainframe, text="Enter start range:")
+labelrange0.pack(side=TOP)
+inputrange0 = Entry(mainframe, width=35, bg=colour6)
+inputrange0.pack(pady = 5)
+
+labelrange1 = Label(mainframe, text="Enter end range:")
+labelrange1.pack(side=TOP)
+inputrange1 = Entry(mainframe, width=35, bg=colour6)
+inputrange1.pack(pady = 5)
 
 mainframe.pack()
 graphframe.pack()
--- stock.py
+++ stock.py
@@ -1,23 +1,25 @@
 import yfinance as yf
 
 class Stock:
-    def getstockprice(self,symbol):
+    def price(self,symbol):
         stockprice = yf.Ticker(symbol.upper()).history(period="1d")
         return stockprice['Close'][0]
 
-    def getstockpricedate(self, yr, mth, day,symbol):
+    def pricedate(self, yr, mth, day,symbol):
         date = str(yr)+"-"+str(mth)+"-"+str(day)
         stockprice = yf.Ticker(symbol.upper()).history(start=date, period="1d")
         return stockprice['Close'][0]
 
     #get stock data for a symbol for a period of time and return the data frame
-    def getstockpricedatayear(self,symbol):
+    def range(self,range0,range1,symbol):
         msft = yf.Ticker(symbol.upper())
         #these dates could be passed as parameters... fogure out how to
 
         #stock_history = msft.history(start="2022-01-01", end="2023-10-31")  
         
-        stock_history = msft.history(start="1985-01-01", end="2023-12-30")  
+
+
+        stock_history = msft.history(start=range0, end=range1)  
         return stock_history
 
     def getstockpricesindaterange(self):
